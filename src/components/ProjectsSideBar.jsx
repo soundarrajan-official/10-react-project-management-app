7b5df5aba9097580291f6d80444622f4029c73ec
soundarrajan-official <soundarrajan.official@gmail.com>
2024-07-03 19:03:16 +0530

    Add Projects Side Bar Component

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,12 @@
+const ProjectsSideBar = () => {
+    return (
+        <aside>
+            <h2>Your Projects</h2>
+            <div>
+                <button>+ Add New Project</button>
+            </div>
+        </aside>
+    )
+}
+
+export default ProjectsSideBar;
